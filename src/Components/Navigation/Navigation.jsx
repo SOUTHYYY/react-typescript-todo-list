03f8418eaad5a916880e14c46a0546a0cb53f5e9
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-02 22:08:42 +0300

    Made it possible to search notes on text

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import ItemStatusFilter from './ItemStatusFilter/ItemStatusFilter';
 import './Navigation.css'
 
-const Navigation = (props) => {
-    return (
-        <div className='search-panel d-flex'>
-            <input className='form-control search-input' placeholder='search'></input>
-            <ItemStatusFilter />
-        </div>
-    )
+class Navigation extends React.Component {
+
+    state = {
+        search: ''
+    }
+
+    onTermChange = (e) => {
+        const {onSearchChange = () => {}} = this.props;
+        this.setState({
+            search: e.target.value
+        })
+        onSearchChange(e.target.value)
+    }
+
+    render(){
+        const {filter, onFilterChange } = this.props
+        return (
+            <div className='search-panel d-flex'>
+                <input
+                    className='form-control search-input' 
+                    placeholder='search'
+                    value={this.state.term}
+                    onChange={this.onTermChange}></input>
+                <ItemStatusFilter filter={filter} onFilterChange={onFilterChange}/>
+            </div>
+        )
+    }
 }
 
 export default Navigation
