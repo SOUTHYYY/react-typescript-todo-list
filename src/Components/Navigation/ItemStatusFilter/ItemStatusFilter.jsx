0646249204cac3873ee68c498faf7cca96d42d47
SOUTHYYY <leshka.l1337@gmail.com>
2019-12-02 22:07:54 +0300

    Made the ability to dynamically change buttons

--- src/Components/Navigation/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/Components/Navigation/ItemStatusFilter/ItemStatusFilter.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ItemStatusFilter.css'
 
 
-export default class ItemStatusFilter extends Component {
-    render() {
+const filterButtons = [
+    { name: 'all', label: 'All' },
+    { name: 'active', label: 'Active' },
+    { name: 'done', label: 'Done' }
+];
+
+const ItemStatusFilter = ({ filter, onFilterChange = () => { } }) => {
+    const buttons = filterButtons.map(({ name, label }) => {
+        const isActive = name === filter;
+        const classNames = 'btn ' + (isActive ? 'btn-info' : 'btn-outline-secondary');
+
         return (
-            <div className='btn-group'>
-                <button type='button' className='btn btn-info'>All</button>
-                <button type='button' className='btn btn-outline-secondary'>Active</button>
-                <button type='button' className='btn btn-outline-secondary'>Done</button>
-            </div>
-        )
-    }
-}
+            <button key={name}
+                type="button"
+                onClick={() => onFilterChange(name)}
+                className={classNames}>{label}</button>
+        );
+    });
+
+    return (
+        <div className="btn-group">
+            {buttons}
+        </div>
+    );
+};
+
+export default ItemStatusFilter
